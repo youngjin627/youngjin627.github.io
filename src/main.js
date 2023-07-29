3bde00dd9f4f57cdf3ca5d227bd9a32c349a93d8
Youngjin627 <140610320+youngjin627@users.noreply.github.com>
2023-07-29 20:29:38 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // Header에 페이지를 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 
-const headerHeight = header. getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 // 위에 줄 자세한 과정
-// const headerRect = header.getBoundingClientRect();
+// const headerRect = header.offsetHeight;
 // console.log(headerRect);
 // const headerHeight = headerRect.height;
 
@@ -17,3 +17,10 @@ document.addEventListener('scroll', /*function*/ () => {    // 이름이 없는 
         header.classList.remove('header--dark');
     }
 });
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
