9f4a6809def17e5b8048979a3e080f1515aa1f9e
Youngjin627 <140610320+youngjin627@users.noreply.github.com>
2023-07-29 21:19:32 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -34,4 +34,16 @@ document.addEventListener('scroll', () => {
     else {
         arrowUp.style.opacity = 0;
     }
-})
+});
+
+// Navbar 토글버튼 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫음
+navbarMenu.addEventListener('click', () => {
+    navbarMenu.classList.remove('open');
+});
